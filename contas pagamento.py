3c5be867b2b8301202f85694ac8ea8bbccb946d7
Henrique Avncini <henriqueavancinitrader@gmail.com>
2022-11-27 18:17:54 -0300

    Python

---
+++ contas pagamento.py
@@ -0,0 +1,10 @@
+compra = 475 * 2
+cartaoItauCard = 570
+aguaLuz = 400
+cartaoC6 = 160
+apt = 820
+manunC_C_Caixa = 12.5*2
+total = compra + aguaLuz+ cartaoC6 + apt+ manunC_C_Caixa + cartaoItauCard
+
+print(total)
+
---
+++ imc.py
@@ -0,0 +1,13 @@
+nome = 'Luis Henrique'
+altura = 1.72
+peso = 65
+imc = peso /(altura * altura)
+linha_1 = f'{nome} tem {altura:.2f} de altura,'
+linha_2 = f'pesa {peso:.2f} e seu IMC é'
+linha_3 = f'{imc:.3f}'
+
+
+print(linha_1)
+print(linha_2)
+print(linha_3)
+##f-strings
