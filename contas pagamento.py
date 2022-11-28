f3eea11a5c51450ba44ae9831bf7536b4e9656c9
Henrique Avncini <henriqueavancinitrader@gmail.com>
2022-11-28 07:04:26 -0300

    #

--- contas pagamento.py
+++ contas pagamento.py
@@ -1,10 +1,37 @@
-compra = 475 * 2
-cartaoItauCard = 570
-aguaLuz = 400
-cartaoC6 = 160
-apt = 820
-manunC_C_Caixa = 12.5*2
-total = compra + aguaLuz+ cartaoC6 + apt+ manunC_C_Caixa + cartaoItauCard
+compra = input ('Quanto gastara com a compra do mercado este mês ?')
+cartaoItauCard = input ('Quanto gastou no cartão itau card ? ')
+faturaLuz = input ('Qual o valor da fatura de Luz ?')
+faturaAgua = input ('Qual o valo da fatura de agua ?')
+cartaoC6 = input ('Deve algo no cartão C6 ?')
+if cartaoC6 == '0':
+    cartaoC6 = input('Gastou quanto no cartão C6 ?')
+else : cartaoC6 == '1'
+cartaoC6 = 0
 
-print(total)
+manunC_C_Caixa = input ('Qual o valor da manutenção da conta da caixa economica ?')
+
+total = compra + cartaoItauCard + faturaLuz + faturaAgua + cartaoC6 + manunC_C_Caixa
+
+print = (' Total :', total)
+
+
+
+
+
+
+
+
+
+
+
+
+##compra = 475 * 2
+##cartaoItauCard = 570
+##aguaLuz = 400
+##cartaoC6 = 160
+##apt = 820
+##manunC_C_Caixa = 12.5*2
+##total = compra + aguaLuz+ cartaoC6 + apt+ manunC_C_Caixa + cartaoItauCard
+
+##print(total)
 
